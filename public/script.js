cc71f3ad545a9bdd6ac51d8ff4e9b8290c43fe2f
Ivan Nasanovich <uolary@gmail.com>
2020-07-03 16:01:55 +0300

    use mongoDB

---
+++ public/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function CreateUser(userName, userAge) {
+        fetch('/api/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: 
+        })
+    }
+});
